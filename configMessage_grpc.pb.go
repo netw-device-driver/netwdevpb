186a31689deaab53c95aa8f95ab5cc1415fd80dc
henderiw <wim.henderickx@gmail.com>
2021-03-21 07:42:32 +0100

    added cache update message

--- configMessage_grpc.pb.go
+++ configMessage_grpc.pb.go
@@ -14,6 +14,92 @@ import (
 // Requires gRPC-Go v1.32.0 or later.
 const _ = grpc.SupportPackageIsVersion7
 
+// CacheUpdateClient is the client API for CacheUpdate service.
+//
+// For semantics around ctx use and closing/ending streaming RPCs, please refer to https://pkg.go.dev/google.golang.org/grpc/?tab=doc#ClientConn.NewStream.
+type CacheUpdateClient interface {
+	Update(ctx context.Context, in *CacheUpdateRequest, opts ...grpc.CallOption) (*CacheUpdateReply, error)
+}
+
+type cacheUpdateClient struct {
+	cc grpc.ClientConnInterface
+}
+
+func NewCacheUpdateClient(cc grpc.ClientConnInterface) CacheUpdateClient {
+	return &cacheUpdateClient{cc}
+}
+
+func (c *cacheUpdateClient) Update(ctx context.Context, in *CacheUpdateRequest, opts ...grpc.CallOption) (*CacheUpdateReply, error) {
+	out := new(CacheUpdateReply)
+	err := c.cc.Invoke(ctx, "/netwdevpb.CacheUpdate/Update", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+// CacheUpdateServer is the server API for CacheUpdate service.
+// All implementations must embed UnimplementedCacheUpdateServer
+// for forward compatibility
+type CacheUpdateServer interface {
+	Update(context.Context, *CacheUpdateRequest) (*CacheUpdateReply, error)
+	mustEmbedUnimplementedCacheUpdateServer()
+}
+
+// UnimplementedCacheUpdateServer must be embedded to have forward compatible implementations.
+type UnimplementedCacheUpdateServer struct {
+}
+
+func (UnimplementedCacheUpdateServer) Update(context.Context, *CacheUpdateRequest) (*CacheUpdateReply, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method Update not implemented")
+}
+func (UnimplementedCacheUpdateServer) mustEmbedUnimplementedCacheUpdateServer() {}
+
+// UnsafeCacheUpdateServer may be embedded to opt out of forward compatibility for this service.
+// Use of this interface is not recommended, as added methods to CacheUpdateServer will
+// result in compilation errors.
+type UnsafeCacheUpdateServer interface {
+	mustEmbedUnimplementedCacheUpdateServer()
+}
+
+func RegisterCacheUpdateServer(s grpc.ServiceRegistrar, srv CacheUpdateServer) {
+	s.RegisterService(&CacheUpdate_ServiceDesc, srv)
+}
+
+func _CacheUpdate_Update_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(CacheUpdateRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(CacheUpdateServer).Update(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/netwdevpb.CacheUpdate/Update",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(CacheUpdateServer).Update(ctx, req.(*CacheUpdateRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+// CacheUpdate_ServiceDesc is the grpc.ServiceDesc for CacheUpdate service.
+// It's only intended for direct use with grpc.RegisterService,
+// and not to be introspected or modified (even as a copy)
+var CacheUpdate_ServiceDesc = grpc.ServiceDesc{
+	ServiceName: "netwdevpb.CacheUpdate",
+	HandlerType: (*CacheUpdateServer)(nil),
+	Methods: []grpc.MethodDesc{
+		{
+			MethodName: "Update",
+			Handler:    _CacheUpdate_Update_Handler,
+		},
+	},
+	Streams:  []grpc.StreamDesc{},
+	Metadata: "configMessage.proto",
+}
+
 // CacheStatusClient is the client API for CacheStatus service.
 //
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://pkg.go.dev/google.golang.org/grpc/?tab=doc#ClientConn.NewStream.
