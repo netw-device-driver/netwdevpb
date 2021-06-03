70c3fb45fa8d04df06954235fb5ebcec02f2a044
henderiw <wim.henderickx@gmail.com>
2021-06-03 20:15:15 +0200

    add deviation

--- configMessage_grpc.pb.go
+++ configMessage_grpc.pb.go
@@ -185,3 +185,89 @@ var CacheStatus_ServiceDesc = grpc.ServiceDesc{
 	Streams:  []grpc.StreamDesc{},
 	Metadata: "configMessage.proto",
 }
+
+// DeviationClient is the client API for Deviation service.
+//
+// For semantics around ctx use and closing/ending streaming RPCs, please refer to https://pkg.go.dev/google.golang.org/grpc/?tab=doc#ClientConn.NewStream.
+type DeviationClient interface {
+	Update(ctx context.Context, in *DeviationUpdate, opts ...grpc.CallOption) (*DeviationUpdateReply, error)
+}
+
+type deviationClient struct {
+	cc grpc.ClientConnInterface
+}
+
+func NewDeviationClient(cc grpc.ClientConnInterface) DeviationClient {
+	return &deviationClient{cc}
+}
+
+func (c *deviationClient) Update(ctx context.Context, in *DeviationUpdate, opts ...grpc.CallOption) (*DeviationUpdateReply, error) {
+	out := new(DeviationUpdateReply)
+	err := c.cc.Invoke(ctx, "/netwdevpb.Deviation/Update", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+// DeviationServer is the server API for Deviation service.
+// All implementations must embed UnimplementedDeviationServer
+// for forward compatibility
+type DeviationServer interface {
+	Update(context.Context, *DeviationUpdate) (*DeviationUpdateReply, error)
+	mustEmbedUnimplementedDeviationServer()
+}
+
+// UnimplementedDeviationServer must be embedded to have forward compatible implementations.
+type UnimplementedDeviationServer struct {
+}
+
+func (UnimplementedDeviationServer) Update(context.Context, *DeviationUpdate) (*DeviationUpdateReply, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method Update not implemented")
+}
+func (UnimplementedDeviationServer) mustEmbedUnimplementedDeviationServer() {}
+
+// UnsafeDeviationServer may be embedded to opt out of forward compatibility for this service.
+// Use of this interface is not recommended, as added methods to DeviationServer will
+// result in compilation errors.
+type UnsafeDeviationServer interface {
+	mustEmbedUnimplementedDeviationServer()
+}
+
+func RegisterDeviationServer(s grpc.ServiceRegistrar, srv DeviationServer) {
+	s.RegisterService(&Deviation_ServiceDesc, srv)
+}
+
+func _Deviation_Update_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(DeviationUpdate)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(DeviationServer).Update(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/netwdevpb.Deviation/Update",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(DeviationServer).Update(ctx, req.(*DeviationUpdate))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+// Deviation_ServiceDesc is the grpc.ServiceDesc for Deviation service.
+// It's only intended for direct use with grpc.RegisterService,
+// and not to be introspected or modified (even as a copy)
+var Deviation_ServiceDesc = grpc.ServiceDesc{
+	ServiceName: "netwdevpb.Deviation",
+	HandlerType: (*DeviationServer)(nil),
+	Methods: []grpc.MethodDesc{
+		{
+			MethodName: "Update",
+			Handler:    _Deviation_Update_Handler,
+		},
+	},
+	Streams:  []grpc.StreamDesc{},
+	Metadata: "configMessage.proto",
+}
